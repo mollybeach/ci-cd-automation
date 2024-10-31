e72fab19732ae4961a82c2719d78f42243af5bb7
mollybeach <mollyjbeach@gmail.com>
2024-10-31 12:05:47 -0700

    test: Add basic API test for root endpoint
    - Implemented initial test suite using Jest and Supertest
    - Added test to verify that GET / returns a welcome message in JSON format
    - Ensured status code 200 and expected response body structure

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('API Tests', () => {
+  test('GET / should return welcome message', async () => {
+    const response = await request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(response.body).toEqual({
+      message: 'Welcome to CI/CD Automation API'
+    });
+  });
+});
